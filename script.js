41c1e613f70ee1e8a0135eb01e3badcad4f75f99
TheoKoder <122385399+TheoKoder@users.noreply.github.com>
2023-04-09 20:45:12 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+function darkModeTog() {
+  var doC= document.documentElement;
+  var cL=doC.classList;
+  cL.toogle('darkmode')
+}
+const tB=document.getElementById('set-theme');
+tB.addEventListener("click",darkModeTog);
